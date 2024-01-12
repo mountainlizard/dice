b9aaa891965284a85c20469f52ddf9788b526272
trepidacious <trepidacious@gmail.com>
2024-01-12 18:27:11 +0000

    Allow varying seed, better styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
+import { useState } from "react";
 import { Dice } from "../lib/components/Dice/Dice";
 
 function App() {
+  const [seed, setSeed] = useState(1);
   return (
     <>
-      <h1>Dice</h1>
-      <div style={{ width: 400, height: 400 }}>
+      <h1 style={{ fontFamily: "sans-serif", color: "wheat" }}>Dice</h1>
+      <div style={{ width: 600, height: 600, background: "#2e262e" }}>
         <Dice
-          size={0.2}
-          count={6}
+          size={0.15}
+          count={9}
           gildedCount={3}
-          seed={1}
+          seed={seed}
           disadvantage={false}
-          desiredRolls={[6, 6, 6, 6, 6, 6]}
+          // desiredRolls={[6, 6, 6, 6, 6, 6]}
         />
       </div>
+      <button onClick={() => setSeed((seed) => seed + 1)}>New roll</button>
     </>
   );
 }
