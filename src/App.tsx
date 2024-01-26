ce2ffda310817ade3b9499bcbedde5f2a2bd5ffb
trepidacious <trepidacious@gmail.com>
2024-01-26 10:50:47 +0000

    Update blend

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
             diceTypes={["D20", "D6", "D10", "D10x10", "D6", "D6"]}
             // diceTypes={["D10x10", "D10"]}
             // diceTypes={range(20).map(() => "D20")}
-            gildedCount={0}
+            gildedCount={3}
             seed={seed}
             disadvantage={false}
             // desiredRolls={[20, 10, 4, 6, 6, 6]}
