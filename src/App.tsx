b93a715513ec8d52a85991a4c9e3c40d0ca56859
trepidacious <trepidacious@gmail.com>
2024-02-05 10:02:44 +0000

    Remove commented code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { Suspense, useState } from "react";
 import { Dice } from "../lib/components/Dice/Dice";
-// import range from "../lib/lib/range";
-// import { DiceAlignment } from "../lib/components/DiceAlignment";
 
 function App() {
   const [seed, setSeed] = useState(1);
@@ -28,17 +26,11 @@ function App() {
               "D12",
               "D20",
             ]}
-            // diceTypes={["D10x10", "D10"]}
-            // diceTypes={range(20).map(() => "D20")}
             gildedCount={7}
             seed={seed}
             disadvantage={false}
-            // desiredRolls={[20, 10, 4, 6, 6, 6]}
             desiredRolls={[1, 2, 3, 4, 50, 6, 7, 4, 6, 8, 9, 90, 12, 20]}
-            // desiredRolls={range(20).map((i) => i + 1)}
-            // desiredRolls={range(20).map(() => 20)}
           />
-          {/* <DiceAlignment /> */}
         </Suspense>
       </div>
       <button onClick={() => setSeed((seed) => seed + 1)}>New roll</button>
