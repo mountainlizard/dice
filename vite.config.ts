5259830e1f14182c1e5dc5d58be86ca6730ac7f6
trepidacious <trepidacious@gmail.com>
2026-08-27 11:18:34 +0100

    Remove unused vite-plugin-lib-inject-css

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import dts from "unplugin-dts/vite";
-import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     dts({ include: "lib", insertTypesEntry: true }),
-    libInjectCss(),
   ],
   build: {
     // do not copy the contents of the public folder to the dist folder
